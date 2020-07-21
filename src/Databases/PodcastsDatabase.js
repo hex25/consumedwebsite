809560a5ecb85debddc4dd6c5baaf289b23ced51
Hex25 <natasha.minin@gmail.com>
2020-07-21 20:32:43 +0200

    3 new releases added

--- src/Databases/PodcastsDatabase.js
+++ src/Databases/PodcastsDatabase.js
@@ -4,16 +4,16 @@
 
 let podcasts = [
     {
-        embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/482901207&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
+        embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/482901207&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
     },
     {
-        embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/717490501&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true'
+        embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/717490501&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true'
     },
     {
-       embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/705811522&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true', 
+       embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/705811522&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true', 
     },
     {
-        embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/710677273&color=%23ff5500&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
+        embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/710677273&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
     },
 
     
