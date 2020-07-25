b599b7e128650eb08d5a389655d544921e968900
Hex25 <natasha.minin@gmail.com>
2020-07-25 19:41:35 +0200

    fixed podcast links

--- src/Databases/PodcastsDatabase.js
+++ src/Databases/PodcastsDatabase.js
@@ -5,89 +5,90 @@
 let podcasts = [
     {
         date: '20200725',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/764535037&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/764535037&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
 
     {
         date: '20170000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/332150996&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/332150996&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20170000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/318923119&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/318923119&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20170000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/315950612&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/315950612&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20170000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/310527419&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/310527419&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20170000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/296409081&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/296409081&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20170000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/291101193&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/291101193&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20170000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/291103138&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/291103138&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20160000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/222229925&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/222229925&color=%2350121a&inverse=false&auto_play=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20180000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/478357536&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/478357536&color=%2350121&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20160000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/257433386&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/257433386&color=%2350121&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20160000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/248662568&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/248662568&color=%2350121&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20160000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/258449257&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/258449257&color=%2350121&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20170000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/290211073&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/290211073&color=%2350121&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20160000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/261016304&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/261016304&color=%2350121&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20150000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/864405655&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/864405655&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20150000',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/864422575&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/864422575&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20192211',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/717490501&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true'
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/717490501&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true'
     },
     {
         date: '20193111',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/705811522&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/705811522&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
         date: '20190711',
-        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/710677273&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/710677273&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
     {
-        date : '20150000',
-        embedUrl : 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/864439492&color=%2350121a&inverse=false&auto_play=false&show_user=true',
+        date: '20150000',
+        embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/864439492&color=%2350121a&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_teaser=false&visual=true',
     },
+   
 
 
 
