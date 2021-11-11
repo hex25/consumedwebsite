851cff359dd29bc66cc2be9e46d4d4be3c19265f
Stellar Works <stellarworks@Natashas-MacBook-Pro.local>
2021-11-12 00:09:46 +0100

    Updates

---
+++ precache-manifest.aa1e7903abf1aff929ed4f939bdb4b47.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb5b836c5e9442554b5712a233d2e97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a8e5343b69146b4139",
+    "url": "/static/css/main.04505d3a.chunk.css"
+  },
+  {
+    "revision": "46f45e1b8a8e5487e515",
+    "url": "/static/js/2.3e145b36.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/static/js/2.3e145b36.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93a8e5343b69146b4139",
+    "url": "/static/js/main.6b57477c.chunk.js"
+  },
+  {
+    "revision": "204b79339c2a0ae82869",
+    "url": "/static/js/runtime-main.3022829f.js"
+  },
+  {
+    "revision": "c800c1f1df66cc1b48a4819801d613bf",
+    "url": "/static/media/Kusanagi.c800c1f1.woff"
+  },
+  {
+    "revision": "c2fa98ad84988f7c1645518b0ed58410",
+    "url": "/static/media/backgroundblack.c2fa98ad.jpeg"
+  },
+  {
+    "revision": "0f7244240d07373e57158dddde836031",
+    "url": "/static/media/cap.0f724424.jpg"
+  },
+  {
+    "revision": "fa32bbe6e3402801bda56c4e90f8e1ab",
+    "url": "/static/media/club.fa32bbe6.jpg"
+  },
+  {
+    "revision": "065fad00f78a0e32c79faad5d0d0a323",
+    "url": "/static/media/hoodie.065fad00.jpg"
+  },
+  {
+    "revision": "3decc5090ffb873a550bfdd9e7c74789",
+    "url": "/static/media/minimal.3decc509.jpg"
+  },
+  {
+    "revision": "7e3fef212117a08f33f4c8f54806d5db",
+    "url": "/static/media/t-shirt.7e3fef21.jpg"
+  },
+  {
+    "revision": "60e095f7ab506393729e27508aa06573",
+    "url": "/static/media/warehouse.60e095f7.jpg"
+  }
+]);
